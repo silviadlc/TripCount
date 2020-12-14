04cc765e7a2e409887ce1ce549dc709345f6d5e5
kokodemo <xnominatorxx@gmail.com>
2020-12-14 15:34:31 +0100

    11/12/20

--- index.php
+++ index.php
@@ -6,23 +6,25 @@
 		<link rel="stylesheet" href="css/colors.css" type="text/css">
 	</head>
 	<body>
-		<div class="Errors">
 
-		</div>
 		<div class="Welcome">
 			<h1 class="WelcomeText">Bienvenido a Trip Cuenta!</h1>
+			<div class="Errors">
+			</div>
 			<div class="SubWelcome">
 				<h3 class="SubWelcomeText">Trip Cuenta es una pagina dedicada a la gestión de gastos en viajes organizados.</h3>
 			</div>
-		</div>
-		<div class="landingButtons">
-			<form method="POST" action="login.php">
-				<input type="submit" name="login" value="Login" class="inputButton">
-			</form>
+			<div class="landingButtons">
+				<form method="POST" action="login.php">
+					<input type="submit" name="login" value="Login" class="inputButton">
+				</form>
 				<img src="Logotripcuenta.png" width="100px" height="50px">
-			<form method="POST" action="register.php">
-				<input type="submit" name="register" value="Register" class="inputButton">
-			</form>
+				<form method="POST" action="register.php">
+					<input type="submit" name="register" value="Register" class="inputButton">
+				</form>
 		</div>
+		</div>
+
+		
 	</body>
 </html>
