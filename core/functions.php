04c47880adf7e6bb0badd93ae6a95573056982ad
edusnchezj <eduardsnchez@gmail.com>
2020-12-15 17:52:34 +0100

    [UPT] Sistema de iniciar sesión

--- core/functions.php
+++ core/functions.php
@@ -1,8 +1,70 @@
 <?php
 	require $_SERVER["DOCUMENT_ROOT"].'/core/connection.php';
+	session_start();
 
 	// TODO: Sistema local para el usuario que haya iniciado sesión
 	$usernameLogged = 2; // ID del usuario local.
+		
+	// Si la variable global de SESSION['alerts'] no esta asignada, se le asigna.
+	if(!isset($_SESSION['alerts'])) {
+		$_SESSION['alerts'] = array();
+	}
+
+	function getTitleDocument() {
+		/**
+		 * Esta función lo que realizará es obtener el título y evitar se tenga que realizar siempre un TITLE.
+		 */
+		$config = array(
+			'/login.php' => 'Iniciar sesión - ',
+			'/home.php' => 'Home - '
+		);
+
+		if(isset($config[parse_url($_SERVER['REQUEST_URI'])['path']])) {
+			return $config[parse_url($_SERVER['REQUEST_URI'])['path']];
+		} else {
+			return 'Página desconocida por el sistema - ';
+		}
+	}
+
+	function getCSSdependingUrl() {
+		/**
+		 * Esta función lo que realizará es obtener CSS y evitar que se acumulen en el HEAD del header.php para evitar conflictos.
+		 */
+		$config = array(
+			'/login.php' => '../css/login.css',
+			'/home.php' => '../css/home.css'
+		);
+
+		return $config[parse_url($_SERVER['REQUEST_URI'])['path']];
+	}
+
+	function notAllowedToEnterIfNotLogged() {
+		/**
+		 * Esta función lo que realizará es no dejar a los usuarios que NO esten logueados a diferentes páginas que pongamos en la variable de $config.
+		 * Cualquiera página que este puesta, se "protegerá" y solo aceptará usuarios logueados si tienen '1' y si tienen '0' solo aceptará usuarios no logueados.
+		 * Página que no este, se indicará como "neutral" y aceptará usuarios logueados y no logueados.
+		 */
+		$config = array(
+			'/home.php' => '1',
+			'/login.php' => '0'
+		);
+
+		if(isset($config[parse_url($_SERVER['REQUEST_URI'])['path']])) {
+			if($config[parse_url($_SERVER['REQUEST_URI'])['path']] == 0) {
+				if(@$_SESSION['userLogged']) {
+					header('Location: /');
+					exit;
+				}
+			} else if($config[parse_url($_SERVER['REQUEST_URI'])['path']] == 1) {
+				if(@!$_SESSION['userLogged']) {
+					header('Location: /');
+					exit;
+				}
+			} else {
+				die('Ha ocurrido un problema en la función y se te ha denegado el acceso.');
+			}
+		}
+	}
 
 	function showTravels($orderBy = 1) {
 		global $conn, $usernameLogged;
@@ -46,3 +108,58 @@
 			return '';
 		}
 	}
+
+	function showAlert($type, $message, $manyErrors = '') {
+        if ($type == 'info'){
+			echo '
+		<div class="alert info">
+			<div class="icon" onclick="closeAlert(this)"><span id="close">&times;</span></div>
+			<div class="alert-content">
+				<h2 class="alert-title">Información</h2>
+				<p>'.$message.'</p>
+			</div>
+		</div>';
+        } elseif ($type == 'danger') {
+			echo '
+		<div class="alert danger">
+			<div class="icon" onclick="closeAlert(this)"><span id="close">&times;</span></div>
+			<div class="alert-content">
+				<h2 class="alert-title">¡Parece que ha habido un problema!</h2>
+				<p>'.$message.'</p>
+			</div>
+		</div>';
+		} elseif ($type == 'danger-m') {
+			echo '
+		<div class="alert danger">
+			<div class="icon" onclick="closeAlert(this)"><span id="close">&times;</span></div>
+			<div class="alert-content">
+				<h2 class="alert-title">¡Parece que ha habido un problema!</h2>
+				<p>'.$message.'</p>
+				<ul>';
+				foreach ($manyErrors as $error) {
+					echo '<li>'.$error.'</li>';
+				}
+				echo '
+				</ul>
+			</div>
+		</div>';
+        } elseif ($type == 'success') {
+			echo '
+		<div class="alert success">
+			<div class="icon" onclick="closeAlert(this)"><span id="close">&times;</span></div>
+			<div class="alert-content">
+				<h2 class="alert-title">¡Éxito!</h2>
+				<p>'.$message.'</p>
+			</div>
+		</div>';
+        } elseif ($type == 'warning') {
+			echo '
+		<div class="alert warning">
+			<div class="icon" onclick="closeAlert(this)"><span id="close">&times;</span></div>
+			<div class="alert-content">
+				<h2 class="alert-title">Aviso</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet lacus et tortor placerat mollis. Nam sapien mauris, rhoncus sit amet dapibus nec, convallis et metus. Praesent ut maximus sem.</p>
+			</div>
+		</div>';
+        }
+    }
--- includes/header.php
+++ includes/header.php
@@ -3,9 +3,14 @@
 	<head>
 		<?php require $_SERVER["DOCUMENT_ROOT"].'/core/functions.php'; ?>
 		<meta charset="utf8">
-		<link rel="stylesheet" href="/css/main.css">
-		<link rel="stylesheet" href="/css/index.css">
+		<title><?php echo getTitleDocument(); ?> Tripcount</title>
+		<?php notAllowedToEnterIfNotLogged(); ?>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="/css/main.css">
+		<link rel="stylesheet" href="../css/colors.css">
+		<link rel="stylesheet" href="<?php echo getCSSdependingUrl(); ?>">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Patrick+Hand&display=swap" rel="stylesheet">   
 		<script src="/js/script.js"></script>
 		<style>
 			header {
