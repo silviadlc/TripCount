cb6df64055db65900f4da14c12dceb67b221bbf3
silviadlc <57746795+silviadlc@users.noreply.github.com>
2020-12-15 15:01:28 +0100

    ADD validation login (in progress)

--- core/login.php
+++ core/login.php
@@ -7,19 +7,47 @@
         <link rel="stylesheet" href="../css/login.css">
         <link rel="stylesheet" href="../css/colors.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Patrick+Hand&display=swap" rel="stylesheet">    </head>
+        <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Patrick+Hand&display=swap" rel="stylesheet">    
+    </head>
+    <?php
+        include "connection.php";
+        include "errors.php";
+
+        $errors = [];
+        $has_error = false;
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['userEmail']) && isset($_POST['userPass'])) {
+
+            $_user = $_POST['userEmail'];
+            $_password = $_POST['userPass'];
+            $passwordEncrypt = password_hash($_password, PASSWORD_DEFAULT);
+
+            $query = $conn -> prepare('SELECT * FROM users WHERE email= ? AND password= ?');
+            $query ->bindParam(1, $_user);
+            $query ->bindParam(2, $_password);
+            $query ->execute();
+            $result = $query -> fetch();
+            
+            if($result != false && password_verify($_password, $passwordEncrypt)){
+                $_SESSION['user'] = $result['isUsername'];
+                errorMessage('success', 'Usuario y password validados'); 
+            } else {
+                errorMessage('error', 'Usuario y password no coinciden');
+            }
+            unset($_POST['userEmail'], $_POST['userPass']);
+        }
+    ?>
     <body>
         <div class="logo">
             <img src="../media/Logotripcuenta.png">
         </div>
 
-        <form action="home.php" method="POST">
+        <form action="login.php" method="POST">
             <h1>INICIAR SESIÓN</h1>
-            <label for="email"><b>Correo electrónico</b></label><br>
-            <input type="email" placeholder="Introduce email" name="email"><br><br>
-            <label for="password"><b>Contraseña</b></label><br>
-            <input type="password" placeholder="Introduce contraseña" name="password"><br><br>
-            <button type="submit">Entrar</button>                
+            <label for="userEmail"><b>Correo electrónico</b></label><br>
+            <input type="email" placeholder="Introduce email" name="userEmail"><br><br>
+            <label for="userPass"><b>Contraseña</b></label><br>
+            <input type="password" placeholder="Introduce contraseña" name="userPass"><br><br>
+            <button type="submit" name="login">Entrar</button>                
         </form>
     </body>
 </html>
