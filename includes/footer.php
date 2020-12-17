0eb00ee970cca3ca383fca1c8e177940907f2c1c
edusnchezj <eduardsnchez@gmail.com>
2020-12-17 02:10:22 +0100

    [UPT] Arreglar texto footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,7 @@
 		<link rel="stylesheet" type="text/css" href="../css/main.css">
 		<div class="footer">
 				<div ><img src="/media/Logotripcuenta.png" width="100px" height="50px"></div>
-				<div><p>Año de fundación: 2020 </p>
-					<p>Año actual: <?php echo date("Y"); ?></p>
+				<div><p>&copy; 2020-<?php echo date("Y"); ?> Equipo T - Todos los derechos reservados</p>
 				</div>
 		</div>
 	</footer>
