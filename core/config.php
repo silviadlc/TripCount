6ca65737b0d51f5f758146713c15f79238a5edcb
silviadlc <57746795+silviadlc@users.noreply.github.com>
2021-01-13 18:12:48 +0100

    added config file

---
+++ core/config.php
@@ -0,0 +1,10 @@
+<?php
+    // Constante que activa mensajes de DEBUG de PHP.
+    define('DEBUG', true);
+
+    // Constantes que configuran la conexión de la base de datos.
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'tripcount-admin');
+    define('DB_PASSWORD', 'pMZ4cIPNim5w%&FZHp9e');
+    define('DB_NAME', 'tripcount');
+?>    
