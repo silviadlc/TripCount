2c2f09535aff0d25472612484ef89e264d83b909
edusnchezj <eduardsnchez@gmail.com>
2021-01-18 19:12:05 +0100

    [FIX] Register [no finalizado]

--- register.php
+++ register.php
@@ -1,52 +1,27 @@
 	<?php	
     include $_SERVER["DOCUMENT_ROOT"].'/includes/header.php';
-            if(isset($_POST["userEmail"])){
-
-                $_SESSION["email"] = $_POST["userEmail"];
-                $email=$_SESSION["email"];
-
-                 //Procederemos a hacer una consulta que buscara el correo del usuario
-                $searchMail = "SELECT count(*) total from users WHERE email='$email'";
-
-                 //Realizamos la consulta y anadimos $connection, ya que es la variable que creamos en nuestro archivo connection.php
-                $result = $conn->query($searchMail);
-                $row = $result->fetchAll(PDO::FETCH_ASSOC);
-                $count = $row['total'];
-                 //SI SI EXISTE una fila, quiere decir QUE SI ESTA EL CORREO EN LA BASE DE DATOS
-                if($count==0) {
-                   header("Location: login.php");
-                }
-            }
 	?>
-<script>
-function passwordCheck(){
-    let passwordValue = document.regist.password.value;
-    let repasswordValue = document.regist.repassword.value;
-
-    if (passwordValue == repasswordValue){
-         document.getElementById('sendRequest').disabled = false;
-    }else{
-         document.getElementById('sendRequest').disabled = true;
-    }
-
-}
-</script>
         <div class="logo">
             <img src="../media/Logotripcuenta.png">
-        </div>
-        <form action="#" method="POST" name="regist">
-            <h1>REGISTRATE!</h1>
-            <?php 
-                if(isset($_SESSION['email'])){
-                    echo'<label for="userEmail"><b>Correo electrónico</b></label><br>
-                    <input type="email" value="'.$email.'" name="userEmail" required><br><br>';
-                }else{
-                    echo'<label for="userEmail"><b>Correo electrónico</b></label><br>
-                    <input type="email" placeholder="Introduce correo electrónico" name="userEmail" required><br><br>';
-            }?>
+		</div>
+		<?php
+			if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sendRegister'])) {
+
+			}
+		?>
+
+        <form action="#" method="POST">
+			<h1>REGISTRATE</h1>
+			<label for="userEmail"><b>Correo electrónico</b></label><br>
+			<input type="email" value="'.$email.'" name="userEmail" required><br><br>
+			<label for="userEmail"><b>Correo electrónico</b></label><br>
+			<input type="email" placeholder="Introduce correo electrónico" name="userEmail" required><br>
+            
             <label for="password"><b>Contraseña</b></label><br>
-            <input type="password" placeholder="Introduce una contraseña" size="20" id="password" name="password" onkeyup="passwordCheck()" required><br><br>
+			<input type="password" placeholder="Introduce una contraseña" size="20" id="password" name="password" onkeyup="passwordCheck()" required><br><br>
+			
             <label for="password"><b>Repetir Contraseña</b></label><br>
-            <input type="password" placeholder="Repite la contraseña" size="20" id="repassword" name="repassword" onkeyup="passwordCheck()" required><br><br>
-            <button type="submit" name="sendRequest" id="sendRequest" disabled>Entrar</button>                
+			<input type="password" placeholder="Repite la contraseña" size="20" id="repassword" name="repassword" onkeyup="passwordCheck()" required><br><br>
+			
+            <input type="submit" name="sendRegister" id="sendRequest"/>              
 		</form>
